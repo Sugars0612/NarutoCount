e89bc6c529fc5c02df02f116ec4b7a190c508847
Sugars0612 <1765770024@qq.com>
2026-07-18 18:06:31 +0800

    fix: 修复 AGP:8.10.1 依赖库版本错误

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,11 +31,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
+    }
+    kotlin {
+        jvmToolchain(17)
     }
 }
 
