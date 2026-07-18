66c8d64ff1f138b3c1a971963cf6fb41203a612f
Sugars0612 <1765770024@qq.com>
2026-07-18 17:53:08 +0800

    chore: 配置hilt基础代码

---
+++ app/src/main/java/com/example/narutocount2/NarutoCountApp.kt
@@ -0,0 +1,7 @@
+package com.example.narutocount2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NarutoCountApp: Application()
---
+++ app/src/main/java/com/example/narutocount2/di/AppModules.kt
@@ -0,0 +1,8 @@
+package com.example.narutocount2.di
+
+import dagger.Module
+
+@Module
+object AppModules {
+
+}
