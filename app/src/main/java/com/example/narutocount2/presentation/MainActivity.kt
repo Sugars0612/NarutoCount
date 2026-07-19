cce7d69d21b7cab19b71d993514b6d219eef90fc
Sugars0612 <1765770024@qq.com>
2026-07-19 15:34:20 +0800

    test: 添加测试代码

--- app/src/main/java/com/example/narutocount2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/narutocount2/presentation/MainActivity.kt
@@ -11,20 +11,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.narutocount2.presentation.main.MainScreen
 import com.example.narutocount2.presentation.ui.theme.NarutoCount2Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NarutoCount2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+
+                MainScreen()
             }
         }
     }
