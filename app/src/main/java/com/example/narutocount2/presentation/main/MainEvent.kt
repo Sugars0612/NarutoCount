59063c899def6d95394fa1f03e2e41d9dff1c43a
Sugars0612 <1765770024@qq.com>
2026-07-19 15:33:00 +0800

    feat: 添加主页面事件类

---
+++ app/src/main/java/com/example/narutocount2/presentation/main/MainEvent.kt
@@ -0,0 +1,7 @@
+package com.example.narutocount2.presentation.main
+
+sealed class MainEvent() {
+    object ClickedWindowButton: MainEvent()
+//    object ClickedConfigureButton: MainEvent()
+//    object ClickedExitButton: MainEvent()
+}
